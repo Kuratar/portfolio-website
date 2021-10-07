f36068d2d5c6fd992e4d469540cbb262dd18b16b
Kuratar <ednkage@gmail.com>
2021-10-07 00:50:43 -0700

    centerd paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,25 @@ const useStyles = makeStyles({
     justifyItems: "center",
     height: "100%",
   },
+  introduction: {
+    margin: "auto",
+    width: "50%",
+  },
 });
 
 export default function About() {
   const classes = useStyles();
 
-  return <div className={classes.page}>about me</div>;
+  return (
+    <div className={classes.page}>
+      <h1>About Me</h1>
+      <div className={classes.introduction}>
+        <p>
+          Hello there! My name is Eric Nguyen Hello there! My name is Eric
+          Nguyen Hello there! My name is Eric Nguyen Hello there! My name is
+          Eric Nguyen
+        </p>
+      </div>
+    </div>
+  );
 }
