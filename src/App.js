acb3608636f812ea46e5834448db46ee9a9ede18
Kuratar <ednkage@gmail.com>
2021-10-07 18:54:24 -0700

    added roboto font and fixed warning in links

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { makeStyles } from "@mui/styles";
 import { Route, Switch} from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import "./App.css";
 
 const useStyles = makeStyles({
   page: {
     backgroundColor: "#242c38",
-    paddingTop: 64
+    paddingTop: 64,
+    fontFamily: "Roboto",
   },
 });
 
--- src/components/Internship.jsx
+++ src/components/Internship.jsx
@@ -13,14 +13,14 @@ export default function Internship() {
   return (
     <div className={classes.internship}>
       <h1>Current Software Intern at Ardent Labs</h1>
-      <a href="https://www.ardentlabs.io/" target="_blank">
+      <a href="https://www.ardentlabs.io/" target="_blank" rel="noreferrer">
         <img
           src="https://static.wixstatic.com/media/132a22_c124961f91294369b46aeaebf0b2182b~mv2.png/v1/fill/w_300,h_60,al_c,q_85/ardent-labs-logo.webp"
           alt="ardent labs logo and link"
         ></img>
       </a>
       <div style={{ marginTop: 25 }}>
-        <a href="https://www.ardentacademy.com/" target="_blank">
+        <a href="https://www.ardentacademy.com/" target="_blank" rel="noreferrer">
           <img
             src="https://www.ardentacademy.com/images/Ardent-logo-50high-hor.png"
             alt="ardent academy logo and link"
